4e4b2382d302c280f43df689ce368a0f0e3f5bfe
BeKa883 <hnbkhanh080803@gmail.com>
2024-03-08 01:24:21 +0700

    update applications

--- src/components/Application/ListApplicationScholarshipProvider.jsx
+++ src/components/Application/ListApplicationScholarshipProvider.jsx
@@ -15,7 +15,7 @@ export default function ListApplicationScholarshipProvider() {
                     console.error("Provider ID not found");
                     return;
                 }
-                
+
                 const applications = await getApplicationsByScholarshipProvider(providerId);
                 setProviderApplications(applications);
             } catch (error) {
@@ -30,21 +30,23 @@ export default function ListApplicationScholarshipProvider() {
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold text-center mb-6">Applications to My Scholarships</h1>
             {providerApplications.length > 0 ? (
-                <ul>
-                    {providerApplications.map((application, index) => (
-                        <li key={index} className="bg-white shadow rounded-lg p-6 mb-4">
-                            <Link href={`/application/${application.applicationId}`} passHref>
-                                <div className="cursor-pointer">
-                                    <h2 className="text-xl font-semibold">{application.scholarshipTitle || "Scholarship Title Not Available"}</h2>
-                                    <p className="text-md text-gray-600">Budget: {application.scholarshipBudget || "N/A"}</p>
-                                    <p className="text-md text-gray-600">Location: {application.scholarshipLocation || "N/A"}</p>
-                                    <p className="text-md text-gray-600">Status: {application.status || "Unknown"}</p>
-                                    <a href={application.Url || "#"} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">Scholarship Details</a>
-                                </div>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+                <div className="flex flex-col w-[70%] mx-auto">
+                    <ul>
+                        {providerApplications.map((application, index) => (
+                            <li key={index} className="bg-white shadow-lg rounded-lg p-6 mb-4 w-full  flex">
+                                <Link href={`/application/${application.applicationId}`} passHref>
+                                    <div className="cursor-pointer">
+                                        <h2 className="text-xl font-semibold">{application.scholarshipTitle || "Scholarship Title Not Available"}</h2>
+                                        <p className="text-md text-gray-600">Budget: {application.scholarshipBudget || "N/A"}</p>
+                                        <p className="text-md text-gray-600">Location: {application.scholarshipLocation || "N/A"}</p>
+                                        <p className="text-md text-gray-600">Status: {application.status || "Unknown"}</p>
+                                        <button href={application.Url || "#"} target="_blank" rel="noopener noreferrer" className="bg-[#3b82f5] hover:bg-[#1d4ed8] text-[#fff] p-2 rounded-md mt-1">Scholarship Details</button>
+                                    </div>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             ) : (
                 <p className="text-center text-gray-500">No applications found.</p>
             )}
